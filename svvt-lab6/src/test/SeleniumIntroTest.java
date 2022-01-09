e954fc955cb71294f7a5826dd443e4c7a65f3271
BasilBosnjak <basil.bosnjak@gmail.com>
2022-01-09 02:12:36 +0100

    Testing site Redirection and Login

--- svvt-lab6/src/test/SeleniumIntroTest.java
+++ svvt-lab6/src/test/SeleniumIntroTest.java
@@ -5,7 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 class SeleniumIntroTest {
@@ -24,13 +26,40 @@ class SeleniumIntroTest {
 		webDriver.close();
 	}
 
+
+//	@Test
+//	// Testing whether the main URL instantly redirects to the login page 
+//	void testRedirect() throws InterruptedException {
+//		webDriver.get("https://admin.staging.wowflow.org");
+//		Thread.sleep(1000);
+//		
+//		String currentUrl = webDriver.getCurrentUrl();
+//		System.out.println("Current URL: " + currentUrl);
+//		assertEquals("https://admin.staging.wowflow.org/login", currentUrl);
+//		Thread.sleep(2000);
+//	}
+	
 	@Test
-	void testTitle() {
-		webDriver.get(baseUrl);
-		String title = webDriver.getTitle();
-		System.out.println("Title: " + title);
+	// Testing whether the login will be successful after inputing the credentials
+	void testLogin() throws InterruptedException {
+		webDriver.get("https://admin.staging.wowflow.org/login");
+		Thread.sleep(2000);
 		
-		assertEquals("University Home - International Burch University", title);
+		WebElement username = webDriver.findElement(By.name("email"));
+		username.sendKeys("adokce+admin2@gmail.com");
+		Thread.sleep(1000);
+		
+		WebElement password = webDriver.findElement(By.name("password"));
+		password.sendKeys("testtest");
+		Thread.sleep(1000);
+		
+		WebElement signIn = webDriver.findElement(By.name("sign in"));
+		signIn.click();
+		Thread.sleep(2000);
+		
+		String Dashboard = webDriver.getCurrentUrl();
+		assertEquals("https://admin.staging.wowflow.org/dashboard/issues", Dashboard);
+		Thread.sleep(2000);
 	}
 
 }
