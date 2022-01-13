475ec42f710cd0adf6e8f400665c0ac48796a680
BasilBosnjak <basil.bosnjak@gmail.com>
2022-01-13 02:24:59 +0100

    Adding products to cart

--- svvt-lab6/src/test/SeleniumIntroTest.java
+++ svvt-lab6/src/test/SeleniumIntroTest.java
@@ -5,10 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.Keys;
 
 class SeleniumIntroTest {
 	private static WebDriver webDriver;
@@ -18,7 +20,7 @@ class SeleniumIntroTest {
 	static void setUpBeforeClass() throws Exception {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Basil\\Desktop\\chromedriver.exe");
 		webDriver = new ChromeDriver();
-		baseUrl = "https://admin.staging.wowflow.org/dashboard/issues";
+		baseUrl = "https://www.demoblaze.com/index.html";
 	}
 
 	@AfterAll
@@ -26,64 +28,102 @@ class SeleniumIntroTest {
 		webDriver.close();
 	}
 
-
+		
 	@Test
-	// Testing whether the login will fail after inputing invalid credentials
-	void falseLogin() throws InterruptedException {
-		webDriver.get("https://admin.staging.wowflow.org/login");
+	//Testing the login
+	void login() throws InterruptedException {
 		webDriver.manage().window().maximize();
+		webDriver.get(baseUrl);
 		Thread.sleep(2000);
 		
-		WebElement username = webDriver.findElement(By.name("email"));
-		username.sendKeys("john.doe@gmail.com");
-		Thread.sleep(1000);
+		WebElement loginButton = webDriver.findElement(By.id("login2"));
+		loginButton.click();
+		Thread.sleep(2000);
 		
-		WebElement password = webDriver.findElement(By.name("password"));
+		WebElement username = webDriver.findElement(By.xpath("//*[@id=\"loginusername\"]"));
+		username.sendKeys("EdinDzeko");
+		Thread.sleep(2000);
+		
+		WebElement password = webDriver.findElement(By.xpath("//*[@id=\"loginpassword\"]"));
 		password.sendKeys("12345678");
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 		
-		WebElement signIn = webDriver.findElement(By.name("sign in"));
-		signIn.click();
-		Thread.sleep(1000);
+		WebElement logIn = webDriver.findElement(By.xpath("//*[@id=\"logInModal\"]/div/div/div[3]/button[2]"));
+		logIn.click();
+		Thread.sleep(2000);
 		
-		String Dashboard = webDriver.getCurrentUrl();
-		assertNotEquals("https://admin.staging.wowflow.org/dashboard/issues", Dashboard);
-		Thread.sleep(1000);
+		assertEquals("Welcome EdinDzeko", webDriver.findElement(By.xpath("/html/body/nav/div[1]/ul/li[7]/a")).getText());
+		Thread.sleep(2000);
 	}
-	
+		
 	@Test
-	// Testing the login after inputing valid credentials
-	void siteLogin() throws InterruptedException {
-		webDriver.get("https://admin.staging.wowflow.org/login");
-		Thread.sleep(1000);
+	//Testing the process of buying products
+	void purchase() throws InterruptedException {
 		
-		WebElement username = webDriver.findElement(By.name("email"));
-		username.sendKeys("adokce+admin2@gmail.com");
-		Thread.sleep(1000);
+		WebElement s6 = webDriver.findElement(By.xpath("/html/body/div[5]/div/div[2]/div/div[1]/div/div/h4/a"));
+		s6.click();
+		Thread.sleep(2000);
 		
-		WebElement password = webDriver.findElement(By.name("password"));
-		password.sendKeys("testtest");
-		Thread.sleep(1000);
+		WebElement cart1 = webDriver.findElement(By.xpath("//*[@id=\"tbodyid\"]/div[2]/div/a"));
+		cart1.click();
+		Thread.sleep(2000);
 		
-		WebElement signIn = webDriver.findElement(By.name("sign in"));
-		signIn.click();
-		Thread.sleep(1000);
+		Alert alertCart = webDriver.switchTo().alert();
+		alertCart.accept();
+		Thread.sleep(1500);
 		
-		String Dashboard = webDriver.getCurrentUrl();
-		assertEquals("https://admin.staging.wowflow.org/dashboard/issues", Dashboard);
-		Thread.sleep(1000);
+		
 	}
-	
-	@Test
-	//Testing the issues priority filter
-	void issues() throws InterruptedException {
-		webDriver.get(baseUrl);
-		Thread.sleep(5000);
 		
-		WebElement allIssues = webDriver.findElement(By.cssSelector("#root > div > div > div.main-wrapper > div.content-areacomponent__ContentAreaWrapper-sc-14l10n2-0.duOsfj.content-area > div > div > div > div.StickyHeader__Wrapper-sc-sr7d0h-0.hInqaX > div:nth-child(2) > div.Sort__SortWrapper-sc-1lteiyd-0.igQbBx > div > button > i"));
-		allIssues.click();
-		Thread.sleep(5000);
-	}
+		
+		
+		
+		
+//		password.sendKeys("12345678");
+//		Thread.sleep(1000);
+//		
+//		WebElement signIn = webDriver.findElement(By.name("sign in"));
+//		signIn.click();
+//		Thread.sleep(1000);
+//		
+//		String Dashboard = webDriver.getCurrentUrl();
+//		assertNotEquals("https://admin.staging.wowflow.org/dashboard/issues", Dashboard);
+//		Thread.sleep(1000);
+//	}
+//	
+//	@Test
+//	// Testing the login after inputing valid credentials
+//	void siteLogin() throws InterruptedException {
+//		webDriver.get("https://admin.staging.wowflow.org/login");
+//		Thread.sleep(1000);
+//		
+//		WebElement username = webDriver.findElement(By.name("email"));
+//		username.sendKeys("adokce+admin2@gmail.com");
+//		Thread.sleep(1000);
+//		
+//		WebElement password = webDriver.findElement(By.name("password"));
+//		password.sendKeys("testtest");
+//		Thread.sleep(1000);
+//		
+//		WebElement signIn = webDriver.findElement(By.name("sign in"));
+//		signIn.click();
+//		Thread.sleep(5000);
+//		
+//		String Dashboard = webDriver.getCurrentUrl();
+//		assertEquals("https://admin.staging.wowflow.org/dashboard/issues", Dashboard);
+//		Thread.sleep(5000);
+//	}
+//	
+//	@Test
+//	//Testing the issues priority filter
+//	void issues() throws InterruptedException {
+//		webDriver.get("https://admin.staging.wowflow.org/dashboard/issues");
+//		Thread.sleep(5000);
+//		
+//		WebElement allIssues = webDriver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div[1]/div/div/div/div[1]/div[1]/div[4]/div/button"));
+//		allIssues.click();
+//		Thread.sleep(5000);
+//	}
 
 
 }
